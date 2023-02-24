e60abfefe1a7ff99f99e9d57f54a2e66b402d228
Mathieu Huot <mathieu.huot@gmail.com>
2023-02-23 22:34:26 -0500

    Removed Number and copyright filters.

--- config/filters.js
+++ config/filters.js
@@ -1,12 +1,4 @@
 module.exports = function(config) {
-  config.addFilter("copyrightYear", function(date) {
-    const initialYear = date ? new Date(date).getFullYear() : new Date().getFullYear();
-    const currentYear = new Date().getFullYear();
-    return (currentYear - initialYear) > 0 ? `${initialYear} - ${currentYear}` : initialYear;
-  });
-  config.addFilter("number", function(value) {
-    return parseInt(value, 10);
-  });
   config.addFilter("loop", function(array, callback) {
     return array.map((item, index, source) => callback(item, index, source)).join("\n");
   });
