3ee70e90167bf7ea2d14e9873bfc0749fa197fb3
Mathieu Huot <mathieu.huot@gmail.com>
2023-02-25 20:41:51 -0500

    Refactored css format process.

--- config/formats.js
+++ config/formats.js
@@ -1,22 +1,24 @@
 const { isProduction } = require("../src/_data/meta");
 const browserslist = require("browserslist");
+const package = require("../package.json");
+const { bundle, browserslistToTargets } = require("lightningcss");
 
 module.exports = function(config) {
   config.addTemplateFormats("css");
   config.addExtension("css", {
     outputFileExtension: "css",
-    async compile(content, path) {
-      if(path !== "./src/assets/style/index.css") return;
-      let { code, map } = (await import("lightningcss")).bundle({
-        filename: path,
+    async compile(inputContent, inputPath) {
+      if(inputPath !== "./src/assets/style/index.css") return;
+      let { code } = bundle({
+        filename: inputPath,
         minify: isProduction ? true : false,
-        targets: (await import("lightningcss")).browserslistToTargets(browserslist("defaults")),
+        targets: browserslistToTargets(browserslist(package.browserslist)),
         drafts: {
           nesting: true
         }
       });
 
-      return (data) => code;
+      return () => code;
     }
   });
 };
