82b652311788bdb09ffa23549ce6ac8665088639
Mathieu Huot <mathieu.huot@gmail.com>
2023-02-28 18:31:11 -0500

    Added manifest.json.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -8,8 +8,19 @@ module.exports = {
   isProduction: Boolean(process.env.PRODUCTION),
   title: "WebC starter kit",
   subtitle: "A kit to start with WebC!",
+  description: "A starter project for Eleventy (11ty) using WebC as a main template langage. Also includes opiniated assets processing and a few more bells and whistles.",
+  display: "fullscreen",
   author: {
     name: "My Name",
     email: "my@email.com"
+  },
+  favicon: {
+    png192: "assets/images/favicon192.png",
+    png512: "assets/images/favicon512.png",
+    ico: ""
+  },
+  color: {
+    theme: "#000",
+    background: "#fff"
   }
 };
