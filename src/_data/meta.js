a4d9bb924ee7898796b858c0e14810a205786616
Mathieu Huot <mathieu.huot@gmail.com>
2023-02-28 00:32:43 -0500

    Converted `isProduction` into a boolean.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -5,7 +5,7 @@ module.exports = {
   feedUrl() {
     return path.join(this.url, "feed.xml");
   },
-  isProduction: process.env.PRODUCTION,
+  isProduction: Boolean(process.env.PRODUCTION),
   title: "WebC starter kit",
   subtitle: "A kit to start with WebC!",
   author: {
