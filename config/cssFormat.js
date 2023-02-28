9d5419459b75eca5205970b3481e2680ba4121d9
Mathieu Huot <mathieu.huot@gmail.com>
2023-02-28 18:30:08 -0500

    Refactored with data parameter.

--- config/cssFormat.js
+++ config/cssFormat.js
@@ -1,7 +1,6 @@
-const { isProduction } = require("../src/_data/meta");
 const browserslist = require("browserslist");
-const package = require("../package.json");
 const { bundle, browserslistToTargets } = require("lightningcss");
+const cssIndex = "./src/assets/style/index.css";
 
 module.exports = function(config) {
   config.addTemplateFormats("css");
@@ -10,12 +9,12 @@ module.exports = function(config) {
     outputFileExtension: "css",
     compile(inputContent, inputPath) {
 
-      if(inputPath !== "./src/assets/style/index.css") return;
+      if(inputPath !== cssIndex) return;
 
-      return () => bundle({
+      return (data) => bundle({
         filename: inputPath,
-        minify: isProduction,
-        targets: browserslistToTargets(browserslist(package.browserslist)),
+        minify: data.meta.isProduction,
+        targets: browserslistToTargets(browserslist(data.pkg.browserslist)),
         drafts: {
           nesting: true
         }
