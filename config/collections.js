73acc8392d385087d4a1798693473d975aa41b32
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-01 20:46:29 -0500

    Added series.

--- config/collections.js
+++ config/collections.js
@@ -1,3 +1,5 @@
+/** @param {import("@11ty/eleventy").UserConfig} config */
+
 module.exports = function(config) {
   config.addCollection("posts", (collection) => {
     const posts = collection.getFilteredByGlob("./src/posts/*.md");
@@ -6,5 +8,15 @@ module.exports = function(config) {
   config.addCollection("pages", (collection) => {
     const pages = collection.getFilteredByGlob("./src/pages/*.webc");
     return pages.sort((a, b) => a.data.navigation.order - b.data.navigation.order);
+  });
+  config.addCollection("series", (collection) => {
+    const posts = collection.getFilteredByGlob("./src/posts/*.md");
+    return posts.reduce((obj, post) => {
+      if(!post.data.series) return obj;
+      if(!obj[post.data.series]) obj[post.data.series] = [];
+
+      obj[post.data.series].push(post);
+      return obj;
+    }, {});
   });
 };
--- src/posts/peace-in-the-city.md
+++ src/posts/peace-in-the-city.md
@@ -3,5 +3,6 @@ title: Peace in the city
 description: This is an essay on peace and how to live toghether.
 lang: en
 published: false
+series: political thinking!
 ---
 Lets be friends.
