1e86dcbf9ce21b41a1c30f1e83cee4c0ded6d8dc
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-05 20:50:30 -0500

    Removed webc rule from posts data file.

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -3,9 +3,6 @@ const { isProduction } = require("../_data/meta");
 module.exports = function() {
   return {
     layout: "article.webc",
-    permalink: (data) => isProduction && !data.published ? false : `posts/${data.page.fileSlug}/`,
-    //webc: {
-      //components: "../../node_modules/@11ty/eleventy-plugin-syntaxhighlight/syntax-highlight.webc"
-    //}
+    permalink: (data) => isProduction && !data.published ? false : `posts/${data.page.fileSlug}/`
   };
 };
