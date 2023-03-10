f4ce735f4b8d2014401c2fd096be284f869b5104
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-10 00:01:25 -0500

    Refactored to an object.

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,11 +1,9 @@
 const meta = require("../_data/meta");
 
-module.exports = function() {
-  return {
-    layout: "article.webc",
-    permalink: (data) => meta.isProduction && !data.published ? false : `posts/${data.page.fileSlug}/`,
-    og_type: "article",
-    og_author: meta.canonical("/about/"),
-    author: meta.author.name
-  };
+module.exports = {
+  layout: "article.webc",
+  permalink: (data) => meta.isProduction && !data.published ? false : `posts/${data.page.fileSlug}/`,
+  og_type: "article",
+  og_author: meta.canonical("/about/"),
+  author: meta.author.name
 };
