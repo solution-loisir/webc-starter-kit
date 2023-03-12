05c05a051787cf641fc4681574288ddbc9597cbc
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-11 22:28:14 -0500

    Indexing blog pagination at one.

--- src/pages/blog.11tydata.js
+++ src/pages/blog.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = {
   permalink(data) {
-    return `blog/${data.pagination.pageNumber ? `page-${data.pagination.pageNumber}/` : ""}`;
+    return `blog/${data.pagination.pageNumber ? `page-${data.pagination.pageNumber + 1}/` : ""}`;
   }
 };
