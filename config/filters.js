fb155cb3fc71e7e6dab7b88876a206e01ab63b35
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-15 17:36:42 -0400

    Lay down the foundation for image-process.

--- config/filters.js
+++ config/filters.js
@@ -5,4 +5,23 @@ module.exports = function(config) {
   config.addFilter("readableDate", function(date) {
     return new Date(date).toLocaleDateString("en-CA", {timeZone: "utc", year: "numeric", month: "numeric", day: "numeric"});
   });
+  config.addFilter("remove", function(exclude) {
+    return {
+      _exclude: typeof exclude === "string" ? [exclude] : exclude,
+      from(source = {}) {
+        const sourceKeys = Object.keys(source);
+        
+        this._exclude.forEach((keyToRemove) => {
+          if(!sourceKeys[keyToRemove]) return;
+          const keyIndex = sourceKeys.findIndex((sourceKey) => sourceKey === keyToRemove);
+          sourceKeys.splice(keyIndex, 1);
+        });
+    
+        return sourceKeys.reduce((acc, current) => {
+          acc[current] = source[current];
+          return acc;
+        }, {});
+      }
+    };
+  });
 };
