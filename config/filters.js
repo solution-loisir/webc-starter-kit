f71166765274e6e0d09a60104c60562f0e81fe17
Mathieu Huot <mathieu.huot@gmail.com>
2023-03-15 20:57:08 -0400

    Deleted `remove

--- config/filters.js
+++ config/filters.js
@@ -5,22 +5,4 @@ module.exports = function(config) {
   config.addFilter("readableDate", function(date) {
     return new Date(date).toLocaleDateString("en-CA", {timeZone: "utc", year: "numeric", month: "numeric", day: "numeric"});
   });
-  config.addFilter("remove", function(exclude) {
-    return {
-      _exclude: typeof exclude === "string" ? [exclude] : exclude,
-      from(source = {}) {
-        const sourceKeys = Object.keys(source);
-
-        this._exclude.forEach((keyToRemove) => {
-          const keyIndex = sourceKeys.findIndex((sourceKey) => sourceKey === keyToRemove);
-          sourceKeys.splice(keyIndex, 1);
-        });
-    
-        return sourceKeys.reduce((acc, current) => {
-          acc[current] = source[current];
-          return acc;
-        }, {});
-      }
-    };
-  });
 };
