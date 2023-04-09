06a62f97d3ed80c107956c0cdae3f73074de9809
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-08 22:31:46 -0400

    Removed chunk utility.

--- config/collections.js
+++ config/collections.js
@@ -1,7 +1,5 @@
 /** @param {import("@11ty/eleventy").UserConfig} config */
 
-const { chunk } = require("./utils");
-
 module.exports = function(config) {
   config.addCollection("posts", (collection) => {
     const posts = collection.getFilteredByGlob("./src/posts/*.md");
