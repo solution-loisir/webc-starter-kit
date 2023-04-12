187483242cc37806c1df85cdc6f0528cfb30eb67
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-11 20:58:01 -0400

    Removed unecessary filters.

--- config/filters.js
+++ config/filters.js
@@ -1,9 +1,6 @@
 const { formatDateUtc } = require("./utils/format-date");
 
 module.exports = function(config) {
-  config.addFilter("dateISOString", function(date) {
-    return new Date(date).toISOString();
-  });
   config.addFilter("readableDate", function(date) {
     return formatDateUtc("YYYY-MM-DD", date);
   });
