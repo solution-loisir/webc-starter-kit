8e0d1131d18a0acd3c28071c3bfe9b81f9447273
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-12 09:35:21 -0400

    Refactored readableDate filter.

--- config/filters.js
+++ config/filters.js
@@ -1,7 +1,7 @@
 const { formatDateUtc } = require("./utils/format-date");
 
 module.exports = function(config) {
-  config.addFilter("readableDate", function(date) {
-    return formatDateUtc("YYYY-MM-DD", date);
+  config.addFilter("readableDate", function(date, format = "YYYY-MM-DD") {
+    return formatDateUtc(format, date);
   });
 };
