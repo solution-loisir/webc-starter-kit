aa91db12d7f4ca0dd31ebcf141d6804499241cc8
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-14 23:11:32 -0400

    Added tagsLimit to meta.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -10,6 +10,7 @@ module.exports = {
   subtitle: "A kit to start with WebC!",
   description: "A starter project for Eleventy (11ty) using WebC as a main template langage. Also includes opiniated assets processing and a few more bells and whistles.",
   display: "fullscreen",
+  tagsLimit: 4,
   author: {
     name: "My Name",
     image: {
