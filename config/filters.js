149e02f8e9a0da9f1f229b1e2aed3351fea645db
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-14 20:43:03 -0400

    Added filters.

--- config/filters.js
+++ config/filters.js
@@ -1,6 +1,7 @@
 const { formatDateUtc } = require("./utils/format-date");
 
 function normalize(value, defaultValue) {
+  // Taken from: https://github.com/mozilla/nunjucks/blob/master/nunjucks/src/filters.js#L8
   if(value === null || value === undefined || value === false) {
     return defaultValue;
   }
@@ -13,6 +14,7 @@ module.exports = function(config) {
   });
 
   config.addFilter("truncate", function(input, length, killwords, end) {
+    // Taken from (slightly modified): https://github.com/mozilla/nunjucks/blob/master/nunjucks/src/filters.js#L522
     input = normalize(input, "");
     length = length || 60;
   
@@ -34,10 +36,29 @@ module.exports = function(config) {
     input += (end !== undefined && end !== null) ? end : "...";
     return input;
   });
-  
+
   config.addFilter("wordcount", function(str) {
+    // Taken from (slightly modified): https://github.com/mozilla/nunjucks/blob/master/nunjucks/src/filters.js#L620
     str = normalize(str, "");
     const words = str ? str.match(/\w+/g) : null;
-    return words ? words.length : null;
+    return words ? words.length : 0;
+  });
+
+  config.addFilter("striptags", function(input, preserveLinebreaks) {
+    // Taken from (slightly modified): https://github.com/mozilla/nunjucks/blob/master/nunjucks/src/filters.js#L489
+    input = normalize(input, "");
+    let tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>|<!--[\s\S]*?-->/gi;
+    let trimmedInput = input.replace(tags, '').trim();
+    let res = "";
+    if (preserveLinebreaks) {
+      res = trimmedInput
+        .replace(/^ +| +$/gm, '') // remove leading and trailing spaces
+        .replace(/ +/g, ' ') // squash adjacent spaces
+        .replace(/(\r\n)/g, '\n') // normalize linebreaks (CRLF -> LF)
+        .replace(/\n\n\n+/g, '\n\n'); // squash abnormal adjacent linebreaks
+    } else {
+      res = trimmedInput.replace(/\s+/gi, " ");
+    }
+    return res;
   });
 };
