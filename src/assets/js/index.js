86aaaab672b1054f7a2f93dd34d6ad71de998f5b
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-25 11:24:11 -0400

    Back to top button still in progress.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,13 @@
-import "@zachleat/details-utils";
+if(document.querySelector('[data-id="top-button"]')) {
+  const toTopButton = document.querySelector('[data-id="top-button"]');
+  const visibilityThreshold = document.body.scrollHeight * 0.3;
+  window.addEventListener("scroll", () => {
+    if(document.body.scrollTop > visibilityThreshold || document.documentElement.scrollTop > visibilityThreshold) {
+      toTopButton.classList.remove("opacity-decrease");
+      toTopButton.classList.add("opacity-increase");
+    } else {
+      toTopButton.classList.remove("opacity-increase");
+      toTopButton.classList.add("opacity-decrease");
+    }
+  });
+}
