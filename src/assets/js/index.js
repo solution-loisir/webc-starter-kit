f78d49e7b3e70437a890454f93a74493d9207c47
Mathieu Huot <mathieu.huot@gmail.com>
2023-04-25 11:53:10 -0400

    Add precision to opacity transition script.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,11 +1,12 @@
 if(document.querySelector('[data-id="top-button"]')) {
   const toTopButton = document.querySelector('[data-id="top-button"]');
-  const visibilityThreshold = document.body.scrollHeight * 0.3;
+  const visibilityThreshold = Math.floor(document.body.scrollHeight * 0.3);
+  
   window.addEventListener("scroll", () => {
     if(document.body.scrollTop > visibilityThreshold || document.documentElement.scrollTop > visibilityThreshold) {
       toTopButton.classList.remove("opacity-decrease");
       toTopButton.classList.add("opacity-increase");
-    } else {
+    } else if(toTopButton.classList.contains("opacity-increase")) {
       toTopButton.classList.remove("opacity-increase");
       toTopButton.classList.add("opacity-decrease");
     }
